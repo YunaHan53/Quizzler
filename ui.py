8630f14d2018e1a4ad3f1822a208c67ba4e1da2c
YunaH <Yunahan53@gmail.com>
2026-02-24 22:48:50 -0500

    Added functionality to display question text on the UI, added functionality to the True/False buttons and compare with correct answer.

--- ui.py
+++ ui.py
@@ -1,9 +1,12 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
 class QuizInterface:
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+
         self.window = Tk()
         self.window.title("Quizzler Trivia")
         self.window.configure(padx=20, pady=20, bg=THEME_COLOR)
@@ -12,6 +15,7 @@ class QuizInterface:
         self.question_text = self.canvas.create_text(
             150,
             125,
+            width= 290,
             text="Trivia Question goes here",
             fill=THEME_COLOR,
             font=("Arial", 14, "italic")
@@ -22,11 +26,27 @@ class QuizInterface:
         self.score_label.grid(row=0, column=1)
         #True/False Buttons
         true_image = PhotoImage(file="images/true.png")
-        self.true_button = Button(image=true_image, highlightthickness=0)
+        self.true_button = Button(image=true_image, highlightthickness=0, command=self.respond_true)
         self.true_button.grid(row=2, column=0)
 
         false_image = PhotoImage(file="images/false.png")
-        self.false_button = Button(image=false_image, highlightthickness=0)
+        self.false_button = Button(image=false_image, highlightthickness=0, command=self.respond_false)
         self.false_button.grid(row=2, column=1)
 
+        self.get_next_question()
+
         self.window.mainloop()
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
+
+    def respond_true(self):
+        user_answer = "True"
+        self.quiz.check_answer(user_answer)
+        self.get_next_question()
+
+    def respond_false(self):
+        user_answer = "False"
+        self.quiz.check_answer(user_answer)
+        self.get_next_question()
